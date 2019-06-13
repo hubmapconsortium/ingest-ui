1177b08c19c986ce846020ea694c49ef247fe415
chb69 <chb69@pitt.edu>
2019-06-13 14:58:24 -0400

    added cross site scripting decorators to the code

--- metadata-api/metadata_endpoint.py
+++ metadata-api/metadata_endpoint.py
@@ -12,8 +12,9 @@ import base64
 from globus_sdk.exc import TransferAPIError
 import sys
 import os
-from metadata import Metadata
+from flask_cors import CORS, cross_origin
 sys.path.append(os.path.realpath("../common-api"))
+from metadata import Metadata
 from hubmap_const import HubmapConst 
 from neo4j_connection import Neo4jConnection
 from uuid_generator import getNewUUID
@@ -39,6 +40,8 @@ def load_config_file():
         app.config['SECRET_KEY'] = config.get('GLOBUS', 'SECRET_KEY')
         app.config['STAGING_FILE_PATH'] = config.get('GLOBUS', 'STAGING_FILE_PATH')
         app.config['PUBLISH_FILE_PATH'] = config.get('GLOBUS','PUBLISH_FILE_PATH')
+        app.config['UUID_UI_URL'] = config.get('HUBMAP', 'UUID_UI_URL')
+        app.config['LOCAL_STORAGE_DIRECTORY'] = config.get('FILE_SYSTEM','LOCAL_STORAGE_DIRECTORY')
         #app.config['DEBUG'] = True
     except OSError as err:
         msg = "OS error.  Check config.ini file to make sure it exists and is readable: {0}".format(err)
@@ -67,11 +70,21 @@ def load_config_file():
         print (msg + "  Program stopped.")
         exit(0)
 
+config = configparser.ConfigParser()
+try:
+    config.read('../common-api/app.properties')
+    app.config['UUID_UI_URL'] = config.get('HUBMAP', 'UUID_UI_URL')
+except:
+    msg = "Unexpected error:", sys.exc_info()[0]
+    print(msg + "  Program stopped.")
+    exit(0)
+
          
 # this method returns a JSON list of the UUIDs for the entities the current user can edit.  The entitytype is an optional parameter.  If it is not set,
 # the method returns all the editable entities available to the user. 
 @app.route('/metadata/usercanedit/type', methods = ['GET'])
 @app.route('/metadata/usercanedit/type/<entitytype>', methods = ['GET'])
+@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['GET'])
 def user_edit_entity_list(entitytype=None):
     token = str(request.headers["AUTHORIZATION"])[7:]
     conn = None
@@ -93,6 +106,7 @@ def user_edit_entity_list(entitytype=None):
 # this method returns a simple JSON message {'editable':'True|False'}.  True indicates that the current
 # user can edit the given entity.  False indicates they cannot edit the entity.
 @app.route('/metadata/usercanedit/<entityuuid>', methods = ['GET'])
+@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['GET'])
 def can_user_edit_entity(entityuuid):
     token = str(request.headers["AUTHORIZATION"])[7:]
     #entityuuid = request.args.get('entityuuid')
@@ -120,6 +134,7 @@ def can_user_edit_entity(entityuuid):
 # The JSON returned looks like {"groupuuid":"5777527e-ec11-11e8-ab41-0af86edb4424", "groupname":"hubmap-all-access"}
 # example url: /metadata/groups/hubmap-read or /metadata/groups/777527e-ec11-11e8-ab41-0af86edb4424  
 @app.route('/metadata/groups/<identifier>', methods = ['GET'])
+@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['GET'])
 def get_group_by_identifier(identifier):
     if len(identifier) == 0:
         abort(400, jsonify( { 'error': 'identifier parameter is required' } ))
@@ -133,6 +148,7 @@ def get_group_by_identifier(identifier):
 
     
 @app.route('/metadata/source/type/<type_code>', methods = ['GET'])
+@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['GET'])
 def get_metadata_by_source_type(type_code):
     if type_code == None or len(type_code) == 0:
         abort(400, jsonify( { 'error': 'type_code parameter is required to get a metadata instance' } ))
@@ -159,6 +175,7 @@ def get_metadata_by_source_type(type_code):
         conn.close()
 
 @app.route('/metadata/source/<uuid>', methods = ['GET'])
+@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['GET'])
 def get_metadata_by_source(uuid):
     if uuid == None or len(uuid) == 0:
         abort(400, jsonify( { 'error': 'uuid parameter is required to get a metadata instance' } ))
@@ -205,6 +222,6 @@ def get_metadata(uuid):
 
 if __name__ == '__main__':
     try:
-        app.run()
+        app.run(port=5002)
     finally:
         pass
