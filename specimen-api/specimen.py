b5fa928d98eff8c79ce5714d0af0a68a801202df
chb69 <chb69@pitt.edu>
2019-06-20 10:53:59 -0400

    fixed a bug when editing a specimen and updating the files

--- specimen-api/specimen.py
+++ specimen-api/specimen.py
@@ -352,6 +352,7 @@ class Specimen:
         return_list = []
         for image_data in image_list:
             try:
+                # upload the file if it represents a new file
                 if image_data['file_name'] in file_list:
                     new_filepath = Specimen.upload_file_data(request, image_data['file_name'], directory_path)
                     desc = ''
@@ -359,8 +360,6 @@ class Specimen:
                         desc = image_data['description']
                     file_obj = {'filepath': new_filepath, 'description': desc}
                     return_list.append(file_obj)
-                else:
-                    raise ValueError('Error: cannot find file: ' + image_data.file_name + ' in the list of files')
             except:
                 raise
         return json.dumps(return_list)
