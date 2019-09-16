a20d2f8ce3deb043747138bdbc855a940f96eb62
etlds <del20@pitt.edu>
2019-09-16 10:55:11 -0400

    cors for /collections

--- datainjest-api/dataset_endpoint.py
+++ datainjest-api/dataset_endpoint.py
@@ -392,7 +392,7 @@ def reopen_dataset(uuid):
         conn.close()
 
 @app.route('/collections', methods = ['GET'])
-@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['GET'])
+@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['GET', 'POST'])
 @secured(groups="HuBMAP-read")
 def get_collections():
     conn = None
@@ -411,8 +411,8 @@ def get_collections():
     finally:
         conn.close()
 
-@app.route('/collections', methods = ['POST'])
-@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['POST'])
+@app.route('/collections', methods = ['GET'])
+@cross_origin(origins=[app.config['UUID_UI_URL']], methods=['POST', 'GET'])
 @secured(groups="HuBMAP-read")
 def create_collection():
     if not request.form:
