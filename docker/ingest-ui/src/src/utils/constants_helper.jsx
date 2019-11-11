6697c20eb3fe95a8ea29f2464158ee51019ffeb3
yuanzhou <yuanzhou19@gmail.com>
2019-11-11 12:37:35 -0500

    Deleted copied src

--- docker/ingest-ui/src/src/utils/constants_helper.jsx
+++
@@ -1,9 +0,0 @@
-export function flattenSampleType(sample_types) {
-  let sample_types_flatten = {};
-
-  sample_types.forEach(group => {
-    Object.assign(sample_types_flatten, group);
-  });
-
-  return sample_types_flatten;
-}
