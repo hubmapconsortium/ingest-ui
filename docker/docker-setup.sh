0ed452d41972fef60759ce7b81c21b479d7ed604
yuanzhou <yuanzhou19@gmail.com>
2019-11-14 10:12:26 -0500

    Fixed .env not being copied

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -6,3 +6,5 @@ mkdir ingest-ui/src
 # Copy over the src folder
 cp -r ../src/ingest-api/* ingest-api/src
 cp -r ../src/ingest-ui/* ingest-ui/src
+# For ingest-ui, also explicitly copy the .env file
+cp ../src/ingest-ui/.env ingest-ui/src
