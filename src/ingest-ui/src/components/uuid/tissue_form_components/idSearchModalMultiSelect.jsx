a44f5b22d69c97ddf26eea7eceea65d11a894927
chb69 <chb69@pitt.edu>
2019-12-11 15:28:10 -0500

    changes in the midst of adding single id selections

---
+++ src/ingest-ui/src/components/uuid/tissue_form_components/idSearchModalMultiSelect.jsx
@@ -0,0 +1,174 @@
+import React, { Component, useState } from "react";
+import Modal from "../modal";
+import axios from "axios";
+import { SAMPLE_TYPES } from "../../../constants";
+import { flattenSampleType } from "../../../utils/constants_helper";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
+//import Paper from '@material-ui/core/Paper';
+import { SelectionState } from '@devexpress/dx-react-grid';
+import {
+  Grid,
+  Table,
+  TableHeaderRow,
+  TableSelection,
+} from '@devexpress/dx-react-grid-material-ui';
+
+
+
+class IDSearchModalMultiSelect extends Component {
+  state = {};
+
+   constructor(props) {
+    super(props);
+    this.group = React.createRef();
+    this.sampleType = React.createRef();
+    this.keywords = React.createRef();
+    this.state = {
+    	columns : [{name: 'hubmap_identifier', title: 'Identifier'}],
+    	rows: this.props.uuid_list};
+  }
+
+  componentDidMount() {
+    this.setState({
+      LookUpShow: false
+    });
+  }
+
+  hideLookUpModal = () => {
+    this.setState({
+      LookUpShow: false
+    });
+  };
+
+  handleSearchClick = () => {
+    const group = this.group.current.value;
+    const sample_type = this.sampleType.current.value;
+    const keywords = this.keywords.current.value;
+    let params = {};
+    params["group"] = group;
+    if (sample_type) {
+      params["specimen_type"] = sample_type;
+    }
+    if (keywords) {
+      params["search_term"] = keywords;
+    }
+
+    const config = {
+      headers: {
+        Authorization:
+          "Bearer " + JSON.parse(localStorage.getItem("info")).nexus_token,
+        "Content-Type": "multipart/form-data"
+      },
+      params: params
+    };
+
+    var specimen_url = `${process.env.REACT_APP_SPECIMEN_API_URL}/specimens/search`;
+    if (this.props.parent === "dataset") {
+      specimen_url = `${process.env.REACT_APP_SPECIMEN_API_URL}/specimens/search?include_datasets=true`;
+    }
+
+    axios
+      .get(`${specimen_url}`, config)
+      .then(res => {
+        let entities = {};
+        if (this.props.parent === "dataset") {
+          res.data.specimens.forEach(s => {
+            if (entities[s.properties.uuid]) {
+              entities[s.properties.uuid].push(s);
+            } else {
+              entities[s.properties.uuid] = [s];
+            }
+          });
+        } else {
+          entities = res.data.specimens;
+        }
+        this.setState({
+          HuBMAPIDResults: Object.values(entities)
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  showSibling = e => {
+    // e.stopPropagation();
+    // this.setState({
+    //   showSibling: !this.state.showSibling
+    // });
+  };
+
+  render() {
+
+        let rows = this.props.uuid_list;  
+        let columns = this.state.columns;  
+		let [selection, setSelection] = rows[0];    
+    return (
+  
+      <Modal show={this.props.show} handleClose={this.props.hide}>
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="card text-center">
+              <div className="card-body">
+                <h5 className="card-title">Select Sibling IDs</h5>
+                <div className="row">
+                  <div className="col-sm-6">
+                    <div className="form-group row">
+
+{rows && (
+      <Grid
+        rows={rows}
+        columns={columns}
+      >
+        <SelectionState
+          selection={selection}
+          onSelectionChange={setSelection}
+        />
+        <Table />
+        <TableHeaderRow />
+        <TableSelection
+          selectByRowClick
+          highlightRow
+          showSelectionColumn={false}
+        />
+      </Grid>
+    )}
+                    </div>
+                  </div>
+                </div>
+                {this.state.HuBMAPIDResults &&
+                  this.state.HuBMAPIDResults.length === 0 && (
+                    <div className="text-center">No record found.</div>
+                  )}
+                <div className="row mb-5">
+                  <div className="col-sm-4 offset-sm-2">
+                    <button
+                      className="btn btn-primary btn-block"
+                      type="button"
+                      onClick={this.handleSearchClick}
+                    >
+                      Search
+                    </button>
+                  </div>
+                  <div className="col-sm-2 text-left">
+                    <button
+                      className="btn btn-outline-secondary btn-block"
+                      type="button"
+                      onClick={this.props.hide}
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    );
+  }
+}
+
+export default IDSearchModalMultiSelect;
