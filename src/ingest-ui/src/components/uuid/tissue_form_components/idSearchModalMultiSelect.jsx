e78d1d09224386df803bafc394cf966d91c04c7d
chb69 <chb69@pitt.edu>
2020-01-07 11:34:53 -0500

    removed some extraneous code

--- src/ingest-ui/src/components/uuid/tissue_form_components/idSearchModalMultiSelect.jsx
+++ src/ingest-ui/src/components/uuid/tissue_form_components/idSearchModalMultiSelect.jsx
@@ -1,8 +1,7 @@
-import React, { Component, useState } from "react";
+import React, { Component} from "react";
 import Modal from "../modal";
 import axios from "axios";
 
-//import Paper from '@material-ui/core/Paper';
 import { SelectionState } from '@devexpress/dx-react-grid';
 import {
   Grid,
@@ -24,6 +23,17 @@ class IDSearchModalMultiSelect extends Component {
     	columns : [{name: 'hubmap_identifier', title: 'Identifier'}],
     	rows: [{hubmap_identifier: 'ID1'},{hubmap_identifier: 'ID2'}]};
     	
+         this.changeSelection = selection => {
+            //this.grid.deselectAll();
+            this.setState({ selection: [0] });
+            this.setState({ selection: selection });
+            this.setState({ selectedRows: []});
+            var newRows = []
+            for (var i=0;i < selection.length; i++) {
+              newRows.push(this.state.rows[selection[i]]);
+            }
+            this.setState({ selectedRows: newRows});
+        };
     
   }
 
@@ -39,96 +49,23 @@ class IDSearchModalMultiSelect extends Component {
     });
   };
 
-  handleSearchClick = () => {
-    const group = this.group.current.value;
-    const sample_type = this.sampleType.current.value;
-    const keywords = this.keywords.current.value;
-    let params = {};
-    params["group"] = group;
-    if (sample_type) {
-      params["specimen_type"] = sample_type;
-    }
-    if (keywords) {
-      params["search_term"] = keywords;
-    }
+  handleDoneClick = () => {
 
-    const config = {
-      headers: {
-        Authorization:
-          "Bearer " + JSON.parse(localStorage.getItem("info")).nexus_token,
-        "Content-Type": "multipart/form-data"
-      },
-      params: params
-    };
-
-    var specimen_url = `${process.env.REACT_APP_SPECIMEN_API_URL}/specimens/search`;
-    if (this.props.parent === "dataset") {
-      specimen_url = `${process.env.REACT_APP_SPECIMEN_API_URL}/specimens/search?include_datasets=true`;
-    }
-
-    axios
-      .get(`${specimen_url}`, config)
-      .then(res => {
-        let entities = {};
-        if (this.props.parent === "dataset") {
-          res.data.specimens.forEach(s => {
-            if (entities[s.properties.uuid]) {
-              entities[s.properties.uuid].push(s);
-            } else {
-              entities[s.properties.uuid] = [s];
-            }
-          });
-        } else {
-          entities = res.data.specimens;
-        }
-        this.setState({
-          HuBMAPIDResults: Object.values(entities)
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
+	this.setState({uuid_list : this.state.selectedRows});
+    this.setState({
+      LookUpShow: false
+    });
   };
-
-  showSibling = e => {
-    // e.stopPropagation();
-    // this.setState({
-    //   showSibling: !this.state.showSibling
-    // });
-  };
-  
-  getRows = () => {
-    if (this.props.uuid_list !== undefined) {
-       return this.props.uuid_list;
-    } else {
-       return [];
-    }
-  }
-  
-  getColumns = () => {
-    if (this.state.columns !== undefined) {
-       return this.state.columns;
-    } else {
-       return [];
-    }
-  }
 
 
   render() {
-        //const [rows] = this.props.uuid_list;  
-       //const  [columns] = this.state.columns;
-       //const  [columns] = React.useState([{name: 'hubmap_identifier', title: 'Identifier'}]);
-       //const [rows] = React.useState([{hubmap_identifier: 'ID1'},{hubmap_identifier: 'ID2'}]);
-	   //const  [selection, setSelection] = React.useState([1]);  
-	   //var columns =   useState(this.state.columns);
-	   //var rows = useState([{hubmap_identifier: 'ID1'},{hubmap_identifier: 'ID2'}]);
-	   //var [selection, setSelection] = useState([1]);
 	   let {rows, columns} = this.state;
 	   let  {selection, setSelection} = rows[0];
         if (this.props.uuid_list === undefined) {
           return(<div></div>);
         } else {
           rows = this.props.uuid_list;
+          this.state.rows = rows;
         }
     return (
   
@@ -142,26 +79,26 @@ class IDSearchModalMultiSelect extends Component {
                   <div className="col-sm-6">
                     <div className="form-group row">
 
-{rows && (
-      <div className="scrollbar-div">
-      <Grid
-        rows={rows}
-        columns={columns}
-      >
-        <SelectionState
-          selection={selection}
-          onSelectionChange={setSelection}
-        />
-        <Table />
-        <TableHeaderRow />
-        <TableSelection
-          selectByRowClick
-          highlightRow
-          showSelectionColumn={false}
-        />
-      </Grid>
-      </div>
-    )}
+					{rows && (
+					      <div className="scrollbar-div">
+					      <Grid
+					        rows={rows}
+					        columns={columns}
+					      >
+					        <SelectionState
+					          selection={selection}
+					          onSelectionChange={this.changeSelection}
+					        />
+					        <Table />
+					        <TableHeaderRow />
+					        <TableSelection
+					          selectByRowClick
+					          highlightRow
+					          showSelectionColumn={false}
+					        />
+					      </Grid>
+					      </div>
+					    )}
                     </div>
                   </div>
                 </div>
@@ -174,9 +111,9 @@ class IDSearchModalMultiSelect extends Component {
                     <button
                       className="btn btn-primary btn-block"
                       type="button"
-                      onClick={this.handleSearchClick}
+                      onClick={this.handleDoneClick}
                     >
-                      Search
+                      Done
                     </button>
                   </div>
                   <div className="col-sm-2 text-left">
