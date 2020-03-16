f4bfae8f8a60fd787dc1d712f53c7f6710464857
yuanzhou <yuanzhou19@gmail.com>
2020-03-16 13:36:40 -0400

    Removed chown -R in entryppint

--- docker/ingest-api-dev/entrypoint.sh
+++ docker/ingest-api-dev/entrypoint.sh
@@ -30,13 +30,6 @@ chown -R hubmap:hubmap /var/log/nginx
 # Specific to PSC hivevm191 dev
 chown -R hubmap:hubmap /etc/pki/nginx
 
-# When running as non-root user, we'll make sure the mounted
-# /hubmap-data directory is owned by hubmap user as well
-chown -R hubmap:hubmap /hubmap-data
-
-# This is the file root dir for assets on dev, test, and prod
-chown -R hubmap:hubmap /usr/src/assets
-
 # Lastly we use gosu to execute our process "$@" as that user
 # Remember CMD from a Dockerfile of child image gets passed to the entrypoint.sh as command line arguments
 # "$@" is a shell variable that means "all the arguments"
