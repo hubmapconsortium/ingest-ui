ff1d23ff73ff22320e7e4efb3655ebfd18efe0f0
Desheng Li <del20@pitt.edu>
2020-04-06 14:09:54 -0400

    test header on create specimen

--- src/ingest-api/app.py
+++ src/ingest-api/app.py
@@ -1141,7 +1141,7 @@ def create_specimen():
             for samples in new_uuid_records['new_samples']:
                 print(f"Begining of reindex {samples['uuid']} call")
                 print(app.config['SEARCH_WEBSERVICE_URL'] + "/reindex/" + samples['uuid'])
-                rspn = requests.put(app.config['SEARCH_WEBSERVICE_URL'] + "/reindex/" + samples['uuid'], headers=request.headers)
+                rspn = requests.put(app.config['SEARCH_WEBSERVICE_URL'] + "/reindex/" + samples['uuid'], headers={'Authorization': request.headers["AUTHORIZATION"]})
                 print(rspn)
                 print(rspn.text)
                 print(f"After reindex {samples['uuid']} call")
