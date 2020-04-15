aa1506f2b7a260801cddc72a043b9fd8ac95ca2e
Desheng Li <del20@pitt.edu>
2020-04-15 16:06:32 -0400

    fix keyboard event swallowed by WebGL problem

--- src/ingest-ui/src/components/uuid/labIdsModal.jsx
+++ src/ingest-ui/src/components/uuid/labIdsModal.jsx
@@ -18,14 +18,14 @@ class LabIDsModal extends Component {
   state = {
     rui_json: "",
     rui_click: {name: ''} ,
-	rui_check: false,
-	rui_view: false,
-	activate_input: true,
-	rui_locations:{name: ''} ,
-	sample_name: ""
+    rui_check: false,
+    rui_view: false,
+    activate_input: true,
+    rui_locations:{name: ''} ,
+    sample_name: ""
   };
   
-  handleRUIJson = (dataFromChild) => {
+  handleRUIJson = dataFromChild => {
 		const {rui_locations} = { ...this.state };
         const currentState = rui_locations; 
         currentState[this.state.sample_name] = dataFromChild;
@@ -45,7 +45,7 @@ class LabIDsModal extends Component {
 		
   };
 
-  handleAddRUILocation = name => e => {
+  handleAddRUILocation = name => {
     const { rui_click} = { ...this.state };
     const currentState = rui_click; 
     currentState[name] = true;
@@ -57,7 +57,7 @@ class LabIDsModal extends Component {
 	});
   };
 
-  openRUIModalHandler = name => () => {
+  openRUIModalHandler = name => {
         this.setState({
             rui_show: true,
             sample_name: name
@@ -108,28 +108,35 @@ class LabIDsModal extends Component {
 	
 	let labIds_locations = [];
     Object.keys(this.state.assigned_ids).map(x => { 
-	  let sample = {};
-	  sample["uuid"] = x;
+      let sample = {};
+      sample["uuid"] = x;
       sample["lab_identifier"] = this.state.assigned_ids[x];
-	  Object.keys(this.state.rui_locations).map(y => { 
-         if (x === y) {
-	        sample["rui_json"] = this.state.rui_locations[y];
-         }
-         return sample;
+      Object.keys(this.state.rui_locations).map(y => { 
+        if (x === y) {
+          sample["rui_json"] = this.state.rui_locations[y];
+        }
+        return sample;
       });
       labIds_locations.push(sample);
+      return;
     });
     return labIds_locations;
   };
 
   handleInputChange = e => {
     const { name, value } = e.target;
+    console.log(name);
+    console.log(value);
     this.setState(prevState => {
       let assigned_ids = Object.assign({}, prevState.assigned_ids);
       assigned_ids[name] = value;
       return { assigned_ids };
     });
   };
+
+  handleInputKeyPress = () => {
+    console.log("Press");
+  }
 
   handleSubmit = () => {
       
@@ -201,24 +208,22 @@ class LabIDsModal extends Component {
                           name={id.uuid}
                           className='form-control'
                           id={id.uuid}
+                          onChange={this.handleInputChange}
                           value={this.state.assigned_ids[id.uuid] || ''}
-						  onChange={this.handleInputChange}
-				      
                         />
                       </div>
 					  <div className="col-sm-2 text-center">
 						<button
 						  type="button"		
-						  onClick={this.handleAddRUILocation(id.uuid)}
+						  onClick={() => this.handleAddRUILocation(id.uuid)}
 						  className="btn btn-primary"
 						>
 						  Register Location
 						</button>
 					  </div>
-			          { this.state.rui_click[id.uuid] && (
-				         <RUIIntegration  jsonRUI = {this.handleRUIJson} />
-				      )}
-					  
+			          {this.state.rui_click[id.uuid] && (
+				         <RUIIntegration handleJsonRUI={this.handleRUIJson} />
+				        )}
 					  { this.state.rui_check && 
 						this.state.rui_locations[id.uuid] !== "" && (
 					    <React.Fragment>
@@ -231,7 +236,7 @@ class LabIDsModal extends Component {
 						   <button
 							 className="btn btn-link"
 							 type="button"
-							 onClick={this.openRUIModalHandler(id.uuid)}
+							 onClick={() => this.openRUIModalHandler(id.uuid)}
 						   >
 						   View 
 						   </button>
--- src/ingest-ui/src/components/uuid/tissue_form_components/ruiIntegration.jsx
+++ src/ingest-ui/src/components/uuid/tissue_form_components/ruiIntegration.jsx
@@ -1,63 +1,68 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
 
 class RUIIntegration extends Component {
   state = {
-    unityInstance:{},
+    unityInstance: {},
     jsonRUI: "",
     rui_back: false,
     close_rui: false,
   };
 
-  componentDidMount() {  
+  componentDidMount() {
     window.hideSignupScreen = true;
-    const unityInstance = window.UnityLoader.instantiate("unityContainer", 
+    const unityInstance = window.UnityLoader.instantiate(
+      "unityContainer",
       "https://cdn.jsdelivr.net/gh/hubmapconsortium/ccf-3d-registration@staging/Build/output.json",
-       {onProgress: window.UnityProgress});
+      { onProgress: window.UnityProgress }
+    );
     this.setState({
-	    unityInstance: unityInstance
-    })
-    
+      unityInstance: unityInstance,
+    });
+
     // Callback when a user submits their RUI data;
     // str is a JSON-encoded string.
     var self = this;
-    window.ruiRegistrationCallback = function(str) {
-      self.setState({jsonRUI: str})
-      self.props.jsonRUI(str);
-	  self.handleCloseScreenClick();
-    }
-                                                    
+    window.ruiRegistrationCallback = function (str) {
+      self.setState({ jsonRUI: str });
+      self.props.handleJsonRUI(str);
+      self.handleCloseScreenClick();
+    };
   }
 
-  handleCloseScreenClick = e => {
+  handleCloseScreenClick = (e) => {
+    this.state.unityInstance.Quit();
     this.setState({
-      close_rui: true
-    })
-  }
+      close_rui: true,
+    });
+  };
 
-  render () {
-	return (
-	  <div className="webgl-content rui">
-	  { !this.state.close_rui && (	
-		 <React.Fragment>         
-        <div id="unityContainer" style={{ width: 1920, height: 1080 }}> 
-        </div>
-        <div className="footer">
-		  <React.Fragment>           
- 			 <div className="webgl-logo"></div>
-           	 <div className = "closeScreen"
-				  onClick={this.handleCloseScreenClick}
-		          >
+  render() {
+    return (
+      <div className='webgl-content rui'>
+        {!this.state.close_rui && (
+          <React.Fragment>
+            <div
+              id='unityContainer'
+              style={{ width: 1920, height: 1080 }}
+            ></div>
+            <div className='footer'>
+              <React.Fragment>
+                <div className='webgl-logo'></div>
+                <div
+                  className='closeScreen'
+                  onClick={this.handleCloseScreenClick}
+                >
                   Close
-             </div>
-			 <div>{this.state.jsonRUI}</div>
-             <div className="title">RUI-Web</div>
+                </div>
+                <div>{this.state.jsonRUI}</div>
+                <div className='title'>RUI-Web</div>
+              </React.Fragment>
+            </div>
           </React.Fragment>
-         </div>
-       </React.Fragment>
-     )}
-     </div>
-     );
-   }
+        )}
+      </div>
+    );
+  }
 }
 
 export default RUIIntegration;
