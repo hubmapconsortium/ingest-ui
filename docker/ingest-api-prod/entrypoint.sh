732d95ab108df9b1112379c74c38e6aacd33b7e1
yuanzhou <yuanzhou19@gmail.com>
2020-07-11 22:19:58 -0400

    Mapping users and groups for POSIC ACL

--- docker/ingest-api-prod/entrypoint.sh
+++ docker/ingest-api-prod/entrypoint.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-# Pass the HOST_UID and HOST_UID from environment variables specified in the child image docker-compose
-HOST_GID=${HOST_GID}
-HOST_UID=${HOST_UID}
+# Pass the needed UIDs and GIDs from environment variables specified in the child image docker-compose
+USER_HIVE_UID=${USER_HIVE_UID}
+USER_HIVE_GID=${USER_HIVE_GID}
+USER_SHIREY_UID=${USER_SHIREY_UID}
+GROUP_HUBMAP_GID=${GROUP_HUBMAP_GID}
+GROUP_HUBSEQ_GID=${GROUP_HUBSEQ_GID}
 
-echo "Starting ingest-api container with the same host user UID: $HOST_UID and GID: $HOST_GID"
+echo "Starting ingest-api container under the mapped host user 'hive' UID: $USER_HIVE_UID and GID: $USER_HIVE_GID"
 
 # Create a new user with the same host UID to run processes on container
 # The Filesystem doesn't really care what the user is called,
 # it only cares about the UID attached to that user
-# Check if user already exists and don't recreate across container restarts
-getent passwd $HOST_UID > /dev/null 2&>1
+# Check if user `hive` already exists and don't recreate across container restarts
+getent passwd $USER_HIVE_UID > /dev/null 2&>1
 # $? is a special variable that captures the exit status of last task
 if [ $? -ne 0 ]; then
-    groupadd -r -g $HOST_GID hubmap
-    useradd -r -u $HOST_UID -g $HOST_GID -m hubmap
+    groupadd -r -g $USER_HIVE_GID hive
+    useradd -r -u $USER_HIVE_UID -g $USER_HIVE_GID -m hive
+fi
+
+# Only create user `shirey` based on the USER_SHIREY_UID, no group
+getent passwd $USER_SHIREY_UID > /dev/null 2&>1
+if [ $? -ne 0 ]; then
+    useradd -r -u $USER_HIVE_UID -m shirey
+fi
+
+# Only create group `hubmap` based on the GROUP_HUBMAP_GID
+getent group $GROUP_HUBMAP_GID > /dev/null 2&>1
+if [ $? -ne 0 ]; then
+    groupadd -r -g $GROUP_HUBMAP_GID hubmap
+fi
+
+# Only create group `hubseq` based on the GROUP_HUBSEQ_GID
+getent group $GROUP_HUBSEQ_GID > /dev/null 2&>1
+if [ $? -ne 0 ]; then
+    groupadd -r -g $GROUP_HUBSEQ_GID hubseq
 fi
 
 # When running Nginx as a non-root user, we need to create the pid file
@@ -22,12 +43,12 @@ fi
 # In individual nginx *.conf, also don't listen on ports 80 or 443 because 
 # only root processes can listen to ports below 1024
 touch /var/run/nginx.pid
-chown -R hubmap:hubmap /var/run/nginx.pid
-chown -R hubmap:hubmap /var/cache/nginx
-chown -R hubmap:hubmap /var/log/nginx
+chown -R hive:hive /var/run/nginx.pid
+chown -R hive:hive /var/cache/nginx
+chown -R hive:hive /var/log/nginx
 
-# Specific to PSC hivevm193 prod
-chown -R hubmap:hubmap /etc/pki/nginx
+# Also make the ssl certificate accessible
+chown -R hive:hive /etc/pki/nginx
 
 # Lastly we use gosu to execute our process "$@" as that user
 # Remember CMD from a Dockerfile of child image gets passed to the entrypoint.sh as command line arguments
