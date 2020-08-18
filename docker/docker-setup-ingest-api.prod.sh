6577b82eab67e6d76d32dc754c6d016f80078d0b
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 10:34:51 -0400

    Remove newlines and spaces from VERSION parse

--- docker/docker-setup-ingest-api.prod.sh
+++ docker/docker-setup-ingest-api.prod.sh
@@ -2,7 +2,7 @@
 
 # Set the version environment variable for the docker build
 # Version number is from the VERSION file
-export INGEST_API_VERSION=`cat ../VERSION`
+export INGEST_API_VERSION=$(tr -d "\n\r" < VERSION | xargs)
 
 echo "INGEST_API_VERSION: $INGEST_API_VERSION"
 
