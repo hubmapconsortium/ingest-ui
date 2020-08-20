569884d7556d0c4c7d4d282c29b934bc80362299
yuanzhou <yuanzhou19@gmail.com>
2020-08-19 23:14:20 -0400

    Added script dir

--- docker/ingest-ui-docker.sh
+++ docker/ingest-ui-docker.sh
@@ -7,6 +7,18 @@ function absent_or_newer () {
     fi
 }
 
+function get_dir_of_this_script () {
+    # This function sets DIR to the directory in which this script itself is found.
+    # Thank you https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
+    SCRIPT_SOURCE="${BASH_SOURCE[0]}"
+    while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SCRIPT_SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" >/dev/null 2>&1 && pwd )"
+        SCRIPT_SOURCE="$(readlink "$SCRIPT_SOURCE")"
+        [[ $SCRIPT_SOURCE != /* ]] && SCRIPT_SOURCE="$DIR/$SCRIPT_SOURCE" # if $SCRIPT_SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" >/dev/null 2>&1 && pwd )"
+}
+
 # Set the version environment variable for the docker build
 # Version number is from the VERSION file
 # Also remove newlines and leading/trailing slashes if present in that VERSION file
@@ -21,6 +33,9 @@ else
     if [[ "$2" != "check" && "$2" != "config" && "$2" != "build" && "$2" != "start" && "$2" != "stop" && "$2" != "down" ]]; then
         echo "Unknown command '$2', specify one of the following: check|config|build|start|stop|down"
     else
+        get_dir_of_this_script
+        echo 'DIR of script:' $DIR
+
         if [ "$2" = "check" ]; then
             # Bash array
             config_paths=(
@@ -29,7 +44,7 @@ else
 
             for pth in "${config_paths[@]}"; do
                 if [ ! -e $pth ]; then
-                    echo "Missing $pth"
+                    echo "Missing file (relative path to DIR of script) :$pth"
                     exit -1
                 fi
             done
