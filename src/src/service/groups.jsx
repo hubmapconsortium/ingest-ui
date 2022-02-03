5a2da04f4a25e21842929807a2c69794352cc8bb
davism84 <midavis@pitt.edu>
2022-02-03 17:31:50 -0500

    fixed permission on buttons for admin, curator and group membership

--- src/src/service/groups.jsx
+++ src/src/service/groups.jsx
@@ -2,6 +2,10 @@
 // https://raw.githubusercontent.com/hubmapconsortium/commons/test-release/hubmap_commons/hubmap-globus-groups.json
 // TEMPORARY FILE FOR GROUPS
 
+
+export const DATA_ADMIN = "89a69625-99d7-11ea-9366-0e98982705c1";
+export const DATA_CURATOR = "75804b96-d4a8-11e9-9da9-0ad4acb67ed4";
+
 export const GROUPS =
 [
 {
