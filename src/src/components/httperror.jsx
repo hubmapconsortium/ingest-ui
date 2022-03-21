697ba940223e879ef2741a359d930fc3f4d88772
Birdie <birdmachine@blackboxjack.com>
2022-03-21 13:51:57 -0400

    add missing file

---
+++ src/src/components/httperror.jsx
@@ -0,0 +1,18 @@
+
+class HTTPError extends Error {
+    constructor(message, status) {
+      super(message);
+      this.status = status;
+    }
+  
+    toJSON() { 
+      return { message: this.message, status: this.status };
+    }
+  }
+  
+  const e = new HTTPError('Fail', 404);
+  // {"nested":{"message":"Fail","status":404},"arr":[{"message":"Fail","status":404}]}
+  console.log(JSON.stringify({
+    nested: e,
+    arr: [e]
+  }));
