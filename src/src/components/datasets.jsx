7e65b7f17811ab1254df4cf3be37b2d5e2d2c7c1
BirdMachine <birdmachine@blackboxjack.com>
2022-08-05 11:07:42 -0400

    Fix Broken Dataset Page Render

--- src/src/components/datasets.jsx
+++ src/src/components/datasets.jsx
@@ -14,7 +14,8 @@ export const RenderDataset = (props) => {
 
   let navigate = useNavigate();
   var [entity_data, setEntity] = useState(null);
-  var [isLoading, setLoading] = useState(true);
+  var [isLoadingEntity, setIsLoadingEntity] = useState(true);
+  var [isLoadingDTList, setIsLoadingDTList] = useState(true);
   var [dataTypeList, setDataTypeList] = useState();
   // var [uuid, setUUID] = useState("");
   var [errorHandler, setErrorHandler] = useState({
@@ -34,15 +35,17 @@ export const RenderDataset = (props) => {
 
     search_api_get_assay_list({"primary": "true"})
     .then((response) => {
-      // console.debug("fetchPrimaryDataTypes Response", response);
+      console.debug("fetchPrimaryDataTypes Response", response);
         let data = response.data;
-        var dt_dict = data.result.map((value, index) => { return value });
+        // console.debug(d);
+        var dt_dict = data.map((value, index) => { return value });
         // console.debug("dt_dict", dt_dict);
         setDataTypeList(dt_dict);
-        setLoading(isLoading++);
+        // setLoading(isLoading++);
+        setIsLoadingDTList(false);
     })
     .catch(error => {
-      // console.debug("fetch DT list Response Error", error);
+      console.debug("fetch DT list Response Error", error);
       passError(error.status, error.response );
     });
 
@@ -54,30 +57,31 @@ export const RenderDataset = (props) => {
           .then((response) => {
               if (response.status === 200) {
                 setEntity(response.results);
-                setLoading(isLoading++); 
+                setIsLoadingEntity(false); 
                 console.debug("fetchEntity Response", response);
                 //console.debug("entity_data", response.results);
               }else{
-                setLoading(isLoading--);
+                // setLoading(isLoading--);
                 console.debug("Non 200 response!");
                 passError(response.status, response.results);
               }
               
             })  
             .catch((error) => {
-              setLoading(isLoading--);
+              setIsLoadingEntity(true);
               console.debug("fetchData Response Error", error);
               passError(error.status, error.response );
             }); 
       };
       fetchEntity(authSet);
     }else{
-      setLoading(isLoading+1);
+      // setLoading(isLoading+1);
+      setIsLoadingEntity(false);
       console.debug("NEW FORM",props);
     }
 
 
-  }, [uuid]);
+  }, [uuid, props]);
 
   function handleCancel(){
     // React wants to simply scroll to the top of the page if we use -1
@@ -94,7 +98,7 @@ export const RenderDataset = (props) => {
 
   function passError(status, message) {
     console.debug("passError Error", status, message);
-    setLoading(false);
+    // setIsLoadingEntity(false);
     setErrorHandler({
         status: status,
         message:message,
@@ -122,17 +126,11 @@ export const RenderDataset = (props) => {
     //     </>
     //   );
     // }else 
-    console.debug(isLoading);
-    if (isLoading <=3) {
-      console.debug("SM");
-        return (
-          <div className="card-body ">
-            <div className="loader">Loading...</div>
-          </div>
-        );
-    }else{
-      console.debug("BG");
-      console.debug("DTLIST", this.props.dataTypeList);
+    if (!isLoadingEntity && !isLoadingDTList ) {
+      console.debug(isLoadingDTList, dataTypeList );
+      console.debug(isLoadingEntity, entity_data);
+      // console.debug("BG");
+      // console.debug("DTLIST", this.props.dataTypeList);
       //console.debug("!isLoading", !isLoading, "errorHandler", errorHandler);
       return (
         <div>
@@ -141,10 +139,17 @@ export const RenderDataset = (props) => {
           handleCancel={handleCancel} 
           editingDataset={entity_data} 
           passError={passError} 
-          dataTypeList={this.props.dataTypeList} 
+          dataTypeList={dataTypeList} 
           />
         </div>
       )
+    }else{
+      console.debug("SM");
+      return (
+        <div className="card-body ">
+          <div className="loader">Loading...</div>
+        </div>
+      );
     }
   }
   
