7d58552302b1c3e81c73292073ac8702734634bb
Birdmachine <jeswaldrip@gmail.com>
2023-02-15 15:23:22 -0500

    Adding bones for Publications

---
+++ src/src/components/publications/publications.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+
+class PublicationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <div> Hiya~! </div>
+      );
+  }
+}
+
+export default PublicationForm;
