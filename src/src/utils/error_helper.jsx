263ca2eaf0b9abe0c3062f55163236ea3c54d485
Birdmachine <jeswaldrip@gmail.com>
2023-04-13 13:05:37 -0400

    Properly wrap Submit Button as seperate from Publish functionality, capture errors from dataset updates through the entity api service, fix missing cancel button

--- src/src/utils/error_helper.jsx
+++ src/src/utils/error_helper.jsx
@@ -15,3 +15,19 @@ export const BuildError = (message) => {
 }
   
 
+export const FormatError = (message) => {
+    console.debug("FormatError", message);
+    var newError;
+    if(message.status && message.status >200){
+        // probably looks like
+        console.debug("FormatError", message.results, message.results.error);
+        // {"status": 400, "results": {"error": "400 Bad Request: The provided status value of Dataset is not valid"}}
+        newError = new Error(message.results);
+    }else{}
+    // Else, let's make it one
+        newError = new Error(message);
+    return (
+        newError
+    )
+}
+  
