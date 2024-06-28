456ae1a33c0adfcc6aa750e8f69724623e50e3de
Birdmachine <jeswaldrip@gmail.com>
2024-06-28 09:51:10 -0400

    Fix Links to Example information

--- src/src/components/metadata.jsx
+++ src/src/components/metadata.jsx
@@ -556,8 +556,10 @@ const introText = () =>{
 
 
 var targetBranch ="master";
-var targetFile = (props.type).slice(0, -1).toLowerCase()
-const exampleFile ="https://raw.githubusercontent.com/hubmapconsortium/dataset-metadata-spreadsheet/main/contributors/latest/contributors.tsv"
+var targetFile = (props.type).toLowerCase()
+// const exampleFile ="https://raw.githubusercontent.com/hubmapconsortium/dataset-metadata-spreadsheet/main/contributors/latest/contributors.tsv"
+const exampleFile ="https://hubmapconsortium.github.io/ingest-validation-tools/sample-"+targetFile+"/current/"
+
   return (
         <div className="row">
           <h4>{toTitleCase(props.type)} Metadata Upload</h4>
