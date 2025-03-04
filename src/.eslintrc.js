0a75719aa855f396e055bc0e73e70ba3f6265051
Birdmachine <jeswaldrip@gmail.com>
2025-03-04 14:45:54 -0500

    Muting ESLint processing to run manually for now

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,49 +1,49 @@
-module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: [
-    // 'standard'
-    'plugin:react/recommended'
-  ],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: [
-    'react'
-  ],
-  rules: {
-    'indent': ['warn', 2],
-    'keyword-spacing': ['warn', {
-      before: false,
-      after: false,
-      overrides: {
-        from: {before: true, after: true},
-        import: {before: false, after: true},
-        static: {after: true}
-      }
-    }],
-    'array-bracket-spacing': ['warn', 'never'],
-    'object-curly-spacing': ['warn', 'never'],
-    'space-before-blocks': ['warn', 'never'],
-    'space-before-function-paren': ['warn', 'never'],
-    'react/no-unescaped-entities': 0, // disable rule
-    'react/prop-types': 0, // disable rule
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true
+//   },
+//   extends: [
+//     // 'standard'
+//     // 'plugin:react/recommended'
+//   ],
+//   overrides: [
+//     {
+//       env: {
+//         node: true
+//       },
+//       files: [
+//         '.eslintrc.{js,cjs}'
+//       ],
+//       parserOptions: {
+//         sourceType: 'script'
+//       }
+//     }
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module'
+//   },
+//   plugins: [
+//     'react'
+//   ],
+//   rules: {
+//     'indent': ['warn', 2],
+//     'keyword-spacing': ['warn', {
+//       before: false,
+//       after: false,
+//       overrides: {
+//         from: {before: true, after: true},
+//         import: {before: false, after: true},
+//         static: {after: true}
+//       }
+//     }],
+//     'array-bracket-spacing': ['warn', 'never'],
+//     'object-curly-spacing': ['warn', 'never'],
+//     'space-before-blocks': ['warn', 'never'],
+//     'space-before-function-paren': ['warn', 'never'],
+//     'react/no-unescaped-entities': 0, // disable rule
+//     'react/prop-types': 0, // disable rule
 		
-  }
-}
+//   }
+// }
