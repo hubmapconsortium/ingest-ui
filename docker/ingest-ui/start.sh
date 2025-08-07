4138435c9ef34969fb1ff28ec57ebcb4352b0103
yuanzhou <zhy19@pitt.edu>
2025-08-07 17:08:15 -0400

    Use abs path

--- docker/ingest-ui/start.sh
+++ docker/ingest-ui/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Replace the custom tags with deployment specific .env
-python ./envs/env_tool.py .env build &
+python /usr/src/app/src/envs/env_tool.py /usr/src/app/src/.env /usr/src/app/src/build &
 
 # Start nginx to serve the static build
 nginx -g 'daemon off;'
