dab95ec6897ed153c4e304d779d21f250dc04eec
yuanzhou <zhy19@pitt.edu>
2025-11-19 12:36:07 -0500

    Use full path of pythn3.13

--- docker/ingest-ui/start.sh
+++ docker/ingest-ui/start.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Replace the custom tags with deployment specific .env
-python /usr/src/app/src/envs/env_tool.py /usr/src/app/src/.env /usr/src/app/src/build &
+# NOTE: Explicitly call the Python executable with its full path instead of just `python` or `python3.13`
+# This is due to the api-base-image v1.2.0 uses aliases
+/usr/local/bin/python3.13 /usr/src/app/src/envs/env_tool.py /usr/src/app/src/.env /usr/src/app/src/build &
 
 # Start nginx to serve the static build
 nginx -g 'daemon off;'
