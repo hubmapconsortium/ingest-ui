ed4fc4b08beb697a35e1db8361a77b34cfcd490d
Birdmachine <jeswaldrip@gmail.com>
2026-01-06 16:02:15 -0500

    css sync &add tooltip to revision links

--- src/src/components/ui/formParts.jsx
+++ src/src/components/ui/formParts.jsx
@@ -356,8 +356,8 @@ function revisionLinksTime(entityData){
         onClick={() => window.open(entityData.entity_type+"/"+entityData.next_revision_uuid, "_blank")}
         variant="caption" >
           <UpdateIcon className="iconEffect" sx={{marginRight: "5px"}}/>
-          This {entityData.entity_type} has a <strong><Typography className="hoverRise" sx={fauxHrefStyle}> next version</Typography> </strong>
-      </Typography>   
+          This {entityData.entity_type} has a <Tooltip title={entityData.hubmap_id }><strong><Typography className="hoverRise" sx={fauxHrefStyle}> next version</Typography> </strong></Tooltip>
+        </Typography>
     )}
     {entityData.previous_revision_uuid &&(
       <Typography 
@@ -367,8 +367,8 @@ function revisionLinksTime(entityData){
         variant="caption" 
         sx={{display: "inline-block", width: "100%"}}>
           <UpdateIcon className="iconEffect"  sx={{transform: "scaleX(-1)", marginRight: "5px"}} />
-          This {entityData.entity_type} has a <strong><Typography className="hoverRise" sx={fauxHrefStyle}> previous version</Typography> </strong>
-      </Typography>   
+          This {entityData.entity_type} has a <Tooltip title={entityData.hubmap_id }><strong><Typography className="hoverRise" sx={fauxHrefStyle}> previous version</Typography> </strong></Tooltip>
+        </Typography> 
     )}
   </>)
 }
