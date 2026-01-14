afb48155038e2984f7f20278cc4446de2db13bf7
Birdmachine <jeswaldrip@gmail.com>
2026-01-14 12:00:34 -0500

    Fix Value type sent to processing & loss of Button Availability after failure

--- src/src/components/forms/Datasets.jsx
+++ src/src/components/forms/Datasets.jsx
@@ -254,7 +254,7 @@ export const DatasetForm = (props) => {
     let selectedUUIDs = bulkSelection.data.map(obj => obj.uuid);
     let cleanForm = {
       lab_dataset_id: form.lab_dataset_id,
-      contains_human_genetic_sequences: form.contains_human_genetic_sequences === "true" ? "Yes" : "No",
+      contains_human_genetic_sequences: (form.contains_human_genetic_sequences === true || form.contains_human_genetic_sequences === "true") ? true : false,
       description: form.description,
       dataset_info: form.dataset_info,
       direct_ancestor_uuids: selectedUUIDs,
@@ -453,7 +453,6 @@ export const DatasetForm = (props) => {
               message: "Process Error - "+statusText+" "+submitErrorResponse,
               status: "error"
             });
-            // console.debug("entity_api_get_entity RESP NOT 200", response.status, response);
           }
         })
         .catch((error) => {
@@ -462,6 +461,7 @@ export const DatasetForm = (props) => {
             message: "Process Error - "+error.toString(),
             status: "error"
           });
+          setLoading(prevVals => ({ ...prevVals, button: { ...prevVals.button, process: false } }));
         });
             
   }
